2ca4240d2b5b9c2c9afd038216ae6cab99c94676
naufalmahardika-12262 <80079011+naufalmahardika-12262@users.noreply.github.com>
2021-03-08 08:28:19 +0700

    Tugas pertemuan ke-1 (PBO Praktikum)

---
+++ tugas1.java
@@ -0,0 +1,25 @@
+public class tugas1 {
+    public static void main(String[] args) {
+
+        //a
+        int a1=-5, a2=8, a3=6;
+        int a = a1+a2*a3;
+        System.out.println(a);
+
+        //b
+        int b1=55, b2=9;
+        int b = (b1+b2)%b2;
+        System.out.println(b);
+
+        //c
+        int c1=20, c2=-3, c3=5;
+        int c = c1+c2*c3/a2;
+        System.out.println(c);
+
+        //d
+        int d1=15, d2=3, d3=2;
+        int d = c3+d1/d2*d3-a2%d2;
+        System.out.println(d);
+
+    }
+}
